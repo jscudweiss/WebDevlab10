5c5773015f62f40c66337b6e66693ffaec7a0ceb
jscudMainDevice <jscudweiss@gmail.com>
2022-04-21 12:54:46 -0400

    form validation on submit

--- public/js/register.js
+++ public/js/register.js
@@ -11,22 +11,31 @@ $('form').on('submit', (() => {
     const pass = $('#password').val();
     const confirm = $('#confirm').val();
     if (!($('#email').val())) {
-        $('#error_msg').text("email must not be empty");
+        $('#error_msg').text("Email must not be empty");
+        return false;
     }
-    if (!()) {
-        $('#error_msg').text("email must not be empty");
+    if (!(pass)) {
+        $('#error_msg').text("Password must not be empty");
+        return false;
+    }
+    if(pass.length<5) {
+        $('#error_msg').text("Password must be at least 5 characters");
+        return false;
     }
     if (!(confirm)) {
         $('#error_msg').text("Confirm password must not be empty");
-    } else {
-        if(confirm !== pass){
-            $('#error_msg').text("Confirm password must not be empty");
-        }
+        return false;
+    }
+    if(confirm !== pass){
+        $('#error_msg').text("Confirm password must match password");
+        return false;
     }
     if (!($('#fullname').val())) {
         $('#error_msg').text("Full Name must not be empty");
+        return false;
     }
     if (!($('#brand').val())) {
         $('#error_msg').text("Brand must not be empty");
+        return false;
     }
 }))
